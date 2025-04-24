a563bfb5065fa1d7b10635c746e9623447366eed
willsather <will@sather.ws>
2025-04-24 16:59:08 -0400

    fix: move web-two routes under /two

--- apps/web-2/src/app/two/.well-known/vercel/flags/route.ts
+++ apps/web-2/src/app/two/.well-known/vercel/flags/route.ts
@@ -2,7 +2,7 @@ import { type ApiData, verifyAccess } from "flags";
 import { getProviderData } from "flags/next";
 import { type NextRequest, NextResponse } from "next/server";
 
-import * as flags from "../../../../lib/flags";
+import * as flags from "../../../../../lib/flags";
 
 export async function GET(request: NextRequest) {
   const access = await verifyAccess(request.headers.get("Authorization"));
