ffc712b40ffe014213fd1ca2fb6e61edf924e92f
willsather <will@sather.ws>
2025-05-22 14:39:55 -0400

    fix: add microfrontends shop config

--- apps/shop/next.config.ts
+++ apps/shop/next.config.ts
@@ -1,7 +1,9 @@
+import { withMicrofrontends } from "@vercel/microfrontends/next/config";
 import { withVercelToolbar } from "@vercel/toolbar/plugins/next";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {};
 
-const withVercelToolbarConfig = withVercelToolbar()(nextConfig);
+const withMicrofrontendsConfig = withMicrofrontends(nextConfig);
+const withVercelToolbarConfig = withVercelToolbar()(withMicrofrontendsConfig);
 export default withVercelToolbarConfig;
